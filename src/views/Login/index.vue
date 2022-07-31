a09375ed07c7e68610ce4b6dd556c2219405639d
caojianwei <2934302822@qq.com>
2022-07-31 11:22:44 +0800

    完成我的页面

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -79,7 +79,7 @@ export default {
         this.$store.commit('SET_TOKEN', token)
 
         // 跳转到profile
-        this.$router.push('/profile')
+        this.$router.push('/my')
 
         this.$toast.success('登录成功！')
 
--- src/views/My/index.vue
+++ src/views/My/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>我的</div>
+  <div>
+    <!-- 头部区域 -->
+    <header>
+      <div class="login" v-if="isLogin">登录后的盒子</div>
+      <div class="logout" v-else>未登录的盒子</div>
+    </header>
+
+    <!-- 中间 -->
+    <main>收藏/历史/消息通知/小智同学</main>
+
+    <!-- 底部 -->
+    <footer>
+      <button v-if="isLogin" @click="logout">退出</button>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    isLogin() {
+      return !!this.$store.state.tokenObj.token
+    }
+  },
+  methods: {
+    logout() {
+      // 清除token
+      this.$store.commit('SET_TOKEN', {})
+    }
+  }
+}
 </script>
 
 <style></style>
