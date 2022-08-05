5a7fcf0c3acc19aa753dae162525d7994629588a
caojianwei <2934302822@qq.com>
2022-08-05 08:38:10 +0800

    完成了channel页面

--- src/api/channel.js
+++ src/api/channel.js
@@ -1,7 +1,55 @@
 import request from '@/utils/request'
+import storage from '@/utils/storage'
 
 export const getMyChannel = () => {
   return request({
     url: '/v1_0/user/channels'
   })
+}
+
+/**
+ * 获取所有频道
+ * @returns Promise
+ */
+export const getAllChannels = () =>
+  request({
+    url: '/v1_0/channels'
+  })
+
+/**
+ * 删除用户的频道
+ * @param {*} target 频道的id
+ * @returns Promise
+ */
+export const delChannel = (target) => {
+  return request({
+    url: `/v1_0/user/channels/${target}`,
+    method: 'DELETE'
+  })
+}
+
+/**
+ * 添加频道
+ * @param {*} id 频道id
+ * @param {*} seq 添加频道的索引值
+ * @returns Promise
+ */
+export const addChannel = (id, seq) => {
+  return request({
+    url: '/v1_0/user/channels',
+    method: 'PATCH',
+    data: {
+      channels: [{ id, seq }]
+    }
+  })
+}
+
+const HEIMA_TOUTIAO_MY_CHANNELS = 'HEIMA_TOUTIAO_MY_CHANNELS'
+
+export const setMyChannelsToLocal = (myChannels) => {
+  storage.set(HEIMA_TOUTIAO_MY_CHANNELS, myChannels)
+}
+
+export const getMyChannelByLocal = () => {
+  return storage.get(HEIMA_TOUTIAO_MY_CHANNELS)
 }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -9,27 +9,77 @@
       <van-tab v-for="item in myChannels" :key="item.id" :title="item.name">
         <article-list :id="item.id"></article-list>
       </van-tab>
-      <span class="toutiao toutiao-gengduo1"></span>
+      <span class="toutiao toutiao-gengduo1" @click="show = true"></span>
     </van-tabs>
+    <!-- 频道弹层 -->
+    <van-popup
+      v-model="show"
+      position="bottom"
+      :style="{ height: '100%' }"
+      closeable
+      close-icon-position="top-left"
+    >
+      <channel-popup
+        :myChannels="myChannels"
+        @close="show = false"
+        @change-active="active = $event"
+        @del-channel="delChannel"
+        @add-channel="addChannel"
+      ></channel-popup>
+    </van-popup>
   </div>
 </template>
 
 <script>
-import { getMyChannel as getMyChannelAPI } from '@/api'
+import {
+  getMyChannel as getMyChannelAPI,
+  delChannel,
+  addChannel,
+  setMyChannelsToLocal,
+  getMyChannelByLocal
+} from '@/api'
 import ArticleList from './component/ArticleList.vue'
+import ChannelPopup from './component/ChannelPopup.vue'
+
 export default {
-  components: { ArticleList },
+  components: {
+    ArticleList,
+    ChannelPopup
+  },
   name: 'Home',
   data() {
     return {
       active: 0,
-      myChannels: []
+      myChannels: [],
+      show: false
     }
   },
   created() {
-    this.getMyChannel()
+    this.initMyChannels()
+  },
+  computed: {
+    isLogin() {
+      return !!this.$store.state.tokenObj.token
+    }
   },
   methods: {
+    initMyChannels() {
+      // 1. 登录的仔
+      // - 发请求 服务器
+      // 2.未登录的仔
+      // - 本地
+      // - 服务器
+      if (this.isLogin) {
+        this.getMyChannel()
+      } else {
+        const myChannels = getMyChannelByLocal()
+        if (myChannels) {
+          this.myChannels = myChannels
+        } else {
+          this.getMyChannel()
+        }
+      }
+    },
     async getMyChannel() {
       try {
         const { data } = await getMyChannelAPI()
@@ -37,6 +87,50 @@ export default {
       } catch (error) {
         console.dir(error)
         this.$toast.fail('获取频道失败，请刷新')
+      }
+    },
+    // 删除频道
+    async delChannel(id) {
+      this.$toast.loading({
+        message: '正在删除频道...',
+        forbidClick: true
+      })
+      try {
+        const newChannels = this.myChannels.filter((item) => item.id !== id)
+        // 删除我的频道
+        if (this.isLogin) {
+          // -1.删除服务器的数据
+          await delChannel(id)
+        } else {
+          setMyChannelsToLocal(newChannels)
+        }
+
+        // -2.删除页面上的数据
+        this.myChannels = newChannels
+
+        this.$toast.success('删除频道成功')
+      } catch (error) {
+        this.$toast.fail('删除频道失败')
+      }
+    },
+    // 添加频道
+    async addChannel(channel) {
+      this.$toast.loading({
+        message: '正在添加频道...',
+        forbidClick: true
+      })
+      try {
+        if (this.isLogin) {
+          await addChannel(channel.id, this.myChannels.length)
+        } else {
+          setMyChannelsToLocal([...this.myChannels, channel])
+        }
+
+        this.myChannels.push(channel)
+
+        this.$toast.success('添加频道成功')
+      } catch (error) {
+        this.$toast.fail('添加频道失败')
       }
     }
   }
@@ -111,6 +205,7 @@ export default {
   text-align: center;
   opacity: 0.6;
   border-bottom: 1px solid #eee;
+  z-index: 999;
 
   position: fixed;
   top: 92px;
